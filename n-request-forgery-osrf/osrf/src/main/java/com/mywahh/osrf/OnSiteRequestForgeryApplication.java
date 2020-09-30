f1b5056093837270f39414bf1d60fbc52f552014
ByronKruger <byronkruger3@gmail.com>
2020-09-30 14:36:22 +0200

    Initial commit of osrf vulnerability.

---
+++ n-request-forgery-osrf/osrf/src/main/java/com/mywahh/osrf/OnSiteRequestForgeryApplication.java
@@ -0,0 +1,13 @@
+package com.mywahh.osrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnSiteRequestForgeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnSiteRequestForgeryApplication.class, args);
+	}
+
+}
---
+++ n-request-forgery-osrf/osrf/src/test/java/com/mywahh/osrf/OnSiteRequestForgeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywahh.osrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnSiteRequestForgeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
