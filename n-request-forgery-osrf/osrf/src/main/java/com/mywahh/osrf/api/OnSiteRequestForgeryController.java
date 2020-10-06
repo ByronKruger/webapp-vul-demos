91ce4245515909ddfc3e0567ee8d018ae5a6e635
ByronKruger <byronkruger3@gmail.com>
2020-10-06 21:13:43 +0200

    finished demonstrating exploitation of OSRF via user-input in the target of a hyperlink in a stored manner.

--- n-request-forgery-osrf/osrf/src/main/java/com/mywahh/osrf/api/OnSiteRequestForgeryController.java
+++ n-request-forgery-osrf/osrf/src/main/java/com/mywahh/osrf/api/OnSiteRequestForgeryController.java
@@ -3,14 +3,26 @@ package com.mywahh.osrf.api;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OnSiteRequestForgeryController {
 
     @GetMapping("/unsafeSearch")
-    public String safeSearchWithSlash(@RequestParam("search") String search, Model model){
+    public String unsafeSearchWithSlash(@RequestParam("search") String search, Model model){
         model.addAttribute("search", search);
         return "search";
+    }
+
+    @PostMapping("/unsafeUsernameUpdate")
+    public String unsafeProfileUsernameUpdate(@RequestParam(name = "username") String username, Model model){
+        model.addAttribute("username", username);
+        return "profile";
+    }
+
+    @GetMapping("/profile")
+    public String getProfile(){
+        return "profile";
     }
 }
