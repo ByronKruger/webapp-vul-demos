1ab2cc6c8f33f943a94569636d140541ce86891c
ByronKruger <byronkruger3@gmail.com>
2020-10-06 20:10:55 +0200

    finished demonstrating exploitation of OSRF via user-input in the target of a hyperlink.

--- n-request-forgery-osrf/osrf/src/main/java/com/mywahh/osrf/api/OnSiteRequestForgeryController.java
+++ n-request-forgery-osrf/osrf/src/main/java/com/mywahh/osrf/api/OnSiteRequestForgeryController.java
@@ -1,34 +1,14 @@
 package com.mywahh.osrf.api;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 @Controller
 public class OnSiteRequestForgeryController {
 
-    @GetMapping("/unsafeSearch/**")
-    public String unsafeSearch(Model model, HttpServletRequest request){
-        String search = request.getRequestURI()
-                .split(request.getContextPath() + "/unsafeSearch/")[1];
-        System.out.println(search);
-        model.addAttribute("search", search);
-        return "search";
-    }
-
-    @GetMapping("/safeSearchWithoutSlash/{search}")
-    public String safeSearchWithoutSlash(@PathVariable String search, Model model){
-        model.addAttribute("search", search);
-        return "search";
-    }
-
-    @GetMapping("/safeSearchWithSlash")
+    @GetMapping("/unsafeSearch")
     public String safeSearchWithSlash(@RequestParam("search") String search, Model model){
         model.addAttribute("search", search);
         return "search";
